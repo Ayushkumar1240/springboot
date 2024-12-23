778878bd035da47db2971e279144b5349845460a
Ayushkumar1240 <ayushkumar4975@gmail.com>
2024-12-23 12:16:34 +0530

    changed for bean-scope

--- src/main/java/com/ayushkumar/springcoredemo/common/TennisCoach.java
+++ src/main/java/com/ayushkumar/springcoredemo/common/TennisCoach.java
@@ -1,9 +1,14 @@
 package com.ayushkumar.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class TennisCoach implements Coach{
+    public TennisCoach(){
+        System.out.println("In Constructor: "+getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
         return "This is a message from TennisCoach";
--- src/main/java/com/ayushkumar/springcoredemo/common/TrackCoach.java
+++ src/main/java/com/ayushkumar/springcoredemo/common/TrackCoach.java
@@ -1,11 +1,15 @@
 package com.ayushkumar.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TrackCoach implements Coach{
+    public TrackCoach(){
+        System.out.println("In Constructor: "+getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
-        return "";
+        return "This is track Coach";
     }
 }
